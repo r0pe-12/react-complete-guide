76614f6c160fd6d3e63b459b67e0a0b019c8466b
Petar Simonovic <simonovicp12@gmail.com>
2023-04-08 20:27:53 +0200

    added some styling

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,19 @@
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
-    return <h2>expense item!</h2>;
+    const expenseDate = new Date(2023, 1, 26);
+    const expenseTitle = 'New Phone';
+    const expenseAmount = 999.99;
+
+    return (
+        <div className={'expense-item'}>
+            <div>{expenseDate.toISOString()}</div>
+            <div className={'expense-item__description'}>
+                <h2>{expenseTitle}</h2>
+                <div className={'expense-item__price'}>${expenseAmount}</div>
+            </div>
+        </div>
+    );
 }
 
 export default ExpenseItem;
