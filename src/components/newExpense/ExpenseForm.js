6fcc3469053813959b3ecc3ddf215197775f43de
Petar Simonovic <simonovicp12@gmail.com>
2023-04-10 00:50:38 +0200

    handling form submission and 2way binding

--- src/components/newExpense/ExpenseForm.js
+++ src/components/newExpense/ExpenseForm.js
@@ -51,20 +51,38 @@ function ExpenseForm() {
         // })
     }
 
+    function formSubmitHandler(e) {
+        e.preventDefault();
+        // console.log(e.target[0].value); // i dont know why are we working with state when we can read data like this
+
+        const userInput = {
+            title: eTitle,
+            amount: eAmount,
+            date: new Date(eDate)
+        };
+
+        setETitle('');
+        setEAmount('');
+        setEDate('');
+
+        console.log(userInput);
+    }
+
     return (
-        <form>
+        <form onSubmit={formSubmitHandler}>
             <div className={'new-expense__controls'}>
                 <div className={'new-expense__control'}>
                     <label>Title</label>
-                    <input type="text" onChange={titleChangeHandler}/>
+                    <input type="text" value={eTitle} onChange={titleChangeHandler}/>
                 </div>
                 <div className={'new-expense__control'}>
                     <label>Amount</label>
-                    <input type="number" min={'0.01'} step={'0.01'} onChange={amountChangeHandler}/>
+                    <input type="number" min={'0.01'} step={'0.01'} value={eAmount} onChange={amountChangeHandler}/>
                 </div>
                 <div className={'new-expense__control'}>
                     <label>Date</label>
-                    <input type="date" min={'2020-01-01'} max={'2023-12-31'} onChange={dateChangeHandler}/>
+                    <input type="date" min={'2020-01-01'} max={'2023-12-31'} value={eDate}
+                           onChange={dateChangeHandler}/>
                 </div>
             </div>
             <div className={'new-expense__actions'}>
